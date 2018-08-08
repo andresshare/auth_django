a3dffec2b84d1104db4a2ec8996e3b1b7af90c5d
andresshare <andresshare@gmail.com>
2018-08-08 18:48:58 -0500

    msg_login

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
 
 def login_user(request):
-    return render(request, 'authenticate/login.html',{})
+
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,('Logged In'))
+            return redirect('home')
+
+
+        else:
+            messages.success(request, (' Error Logged In *Try again'))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html',{})
