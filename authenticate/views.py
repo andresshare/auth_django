20cfbe82f4e57bfc86bc411771244362a14359cd
andresshare <andresshare@gmail.com>
2018-08-08 22:10:51 -0500

    urls,views,auth

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'authenticate/home.html', {})
@@ -27,3 +28,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, (' you have been Logged Out'))
     return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('you have registered'))
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+        context = {'form':form}
+    return render(request, 'authenticate/register.html',context )
